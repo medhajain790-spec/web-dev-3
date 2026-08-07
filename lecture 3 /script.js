f8b0b9af7386e108e7eabfc263611203c1efb592
Medha Jain <medhajain@Medhas-MacBook-Air.local>
2026-08-07 11:41:29 +0530

    first commit

---
+++ lecture 3 /script.js
@@ -0,0 +1,12 @@
+// const math = require('./math.js');
+
+
+
+// console.log(math.substract(5,3));
+
+// console.log("Hello World");
+
+import {add,substract}  from './math.js';
+
+console.log(add(5,3));
+console.log(substract(5,3));
